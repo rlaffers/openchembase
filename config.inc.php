fff9757d0892ee31cc0e9f64e1dfc3e3e4299f2a
Richard Laffers <rlaffers@gmail.com>
2010-11-13 21:34:21 +0100

    New config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package openchembase
+ * @author Richard Laffers
+ *
+ * Configuration file. *
+ *
+ */
+$cfg = array(
+    'db_server' => 'localhost',
+    'db_user' => 'your_user',
+    'db_password' => 'your_password',
+    'db_database' => 'your_database',
+    );
+?>
